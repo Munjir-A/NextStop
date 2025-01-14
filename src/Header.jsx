aeecd856d2e238775b05322599b687d0f6c7bdf4
Munjir-A <munjiranoar@gmail.com>
2025-01-14 23:10:39 +1100

    added background image

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,17 @@
 function Header(){
     return(
-        <div className="HeaderTitle">
-            <h1>NextStop</h1>
+        <div className="Header">
+            <a className="HeaderTitle">NextStop</a>
+            <nav>
+                <ul className="HeaderNavigations">
+                    <li><a className="nav" href="#">About</a></li>
+                    <li><a className="nav" href="#">Tracker</a></li>
+                    <li><a className="nav" href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <a className="Login" href="#"><button>Login</button></a>
         </div>
+
     );
 }
 export default Header
