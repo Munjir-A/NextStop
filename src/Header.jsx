76d406a31237ff5a135e31c6f50b0c509caf98fa
Munjir-A <munjiranoar@gmail.com>
2025-01-21 18:22:23 +1100

    created header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,16 @@
 function Header(){
     return(
-        <div className="Header">
-            <a className="HeaderTitle">NextStop</a>
+        <header>
+            <a className="HeaderTitle" href="index.html">NextStop</a>
             <nav>
                 <ul className="HeaderNavigations">
-                    <li><a className="nav" href="#">About</a></li>
+                    <li><a className="nav" href="">About</a></li>
                     <li><a className="nav" href="#">Tracker</a></li>
                     <li><a className="nav" href="#">Contact</a></li>
                 </ul>
             </nav>
             <a className="Login" href="#"><button>Login</button></a>
-        </div>
+        </header>
 
     );
 }
