3483346e4a5f30b0b284a9e052055de63aa06ef4
Munjir-A <munjiranoar@gmail.com>
2025-01-24 09:14:32 +1100

    working on welcome text animation

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,8 +1,6 @@
 import {SignedIn, SignedOut, SignInButton, SignUpButton, UserButton, useUser} from "@clerk/clerk-react"
-
 import "./authbutton.css";
 function Auth() {
-    const {user} = useUser();
     return(
         <>
             <div className="sign-in-container">
@@ -15,18 +13,7 @@ function Auth() {
                 </SignedIn>
 
             </div>
-            <div className="WelcomeDisplay">
-                <SignedIn>
-                    <h1>
-                        Welcome {user?.firstName}! Start Tracking Your Internships.
-                    </h1>
-                </SignedIn>
-                <SignedOut>
-                    <h1>
-                        Welcome to NextStop! Start Tracking Your Internships.
-                    </h1>
-                </SignedOut>
-            </div>
+
             </>  
     );
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,18 +1,33 @@
+import {SignedIn, SignedOut, SignInButton, SignUpButton, UserButton, useUser} from "@clerk/clerk-react"
 import Auth from "../auth";
 function Home(){
+    const {user} = useUser();
     return(
-        <header>
-            <a className="HeaderTitle" href="./">NextStop</a>
-            <nav>
-                <ul className="HeaderNavigations">
-                    <li><a className="nav" href="about.html">About</a></li>
-                    <li><a className="nav" href="tracker.html">Tracker</a></li>
-                    <li><a className="nav" href="contact.html">Contact</a></li>
-                </ul>
-            </nav>
-            <a className="Login"><Auth/></a>
-        </header>
-
+        <>
+            <header>
+                <a className="HeaderTitle" href="./">NextStop</a>
+                <nav>
+                    <ul className="HeaderNavigations">
+                        <li><a className="nav" href="about.html">About</a></li>
+                        <li><a className="nav" href="tracker.html">Tracker</a></li>
+                        <li><a className="nav" href="contact.html">Contact</a></li>
+                    </ul>
+                </nav>
+                <a className="Login"><Auth/></a>
+            </header>
+            <div className="WelcomeDisplay">
+                <SignedIn>
+                    <a>
+                        Welcome {user?.firstName}! Start Tracking Your Internships.
+                    </a>
+                </SignedIn>
+                <SignedOut>
+                    <a>
+                        Welcome to NextStop! Start Tracking Your Internships.
+                    </a>
+                </SignedOut>
+            </div>
+        </>
     );
 }
 export default Home
