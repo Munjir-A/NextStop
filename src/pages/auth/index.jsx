e52b47ebc3b218f256f8d5a409ad04933bdf191b
Munjir-A <munjiranoar@gmail.com>
2025-01-23 23:18:55 +1100

    Welcome text with conditions if signed in or not and displaying first name

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,17 +1,33 @@
-import {SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from "@clerk/clerk-react"
+import {SignedIn, SignedOut, SignInButton, SignUpButton, UserButton, useUser} from "@clerk/clerk-react"
+
 import "./authbutton.css";
 function Auth() {
+    const {user} = useUser();
     return(
-        <div className="sign-in-container">
-            <SignedOut>
-                <SignUpButton className="signup" mode="modal"/>
-                <SignInButton className="signin"mode="modal">Login</SignInButton> 
-            </SignedOut> 
-            <SignedIn>
-                <UserButton className="userimg"/>
-            </SignedIn>
+        <>
+            <div className="sign-in-container">
+                <SignedOut>
+                    <SignUpButton className="signup" mode="modal"/>
+                    <SignInButton className="signin"mode="modal">Login</SignInButton> 
+                </SignedOut> 
+                <SignedIn>
+                    <UserButton className="userimg"/>
+                </SignedIn>
 
-        </div>  
+            </div>
+            <div className="WelcomeDisplay">
+                <SignedIn>
+                    <h1>
+                        Welcome {user?.firstName}! Start Tracking Your Internships.
+                    </h1>
+                </SignedIn>
+                <SignedOut>
+                    <h1>
+                        Welcome to NextStop! Start Tracking Your Internships.
+                    </h1>
+                </SignedOut>
+            </div>
+            </>  
     );
 }
 
